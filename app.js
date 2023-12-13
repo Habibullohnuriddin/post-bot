33860b7904013dc0236f0473c6ef906bbd4db60f
Habibulloh <nuriddinovhabibulloh@gmail.com>
2023-12-14 00:35:19 +0500

    mayda kamchiliklar tuzatildi va matnlarga o'zgartirishlar kiritildi

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-require('dotenv').config()
-require("mongoose")
-require('./controllers')
-require('./core')
+require("dotenv").config();
+require("mongoose");
+require("./controllers");
+require("./core");
 
-const mongoose = require('mongoose');
-const cors = require('cors')
-const express = require('express');
+const mongoose = require("mongoose");
+const cors = require("cors");
+const express = require("express");
 const app = express();
 
 // const uri = process.env.MONGO_URI
@@ -14,14 +14,17 @@ app.use(express.json());
 app.use(cors());
 
 try {
-  mongoose.connect('mongodb+srv://nuriddin_off:ju_YC3Y6fA6gsY7@creator-post.elqiuol.mongodb.net/peoples?retryWrites=true&w=majority', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    serverSelectionTimeoutMS: 20000,
-  });
+  mongoose.connect(
+    "mongodb+srv://nuriddin_off:ju_YC3Y6fA6gsY7@creator-post.elqiuol.mongodb.net/peoples?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      serverSelectionTimeoutMS: 20000,
+    }
+  );
   app.listen(() => {
     console.log(`✅ MongoDB-ga ulanish muvaffaqiyatli amalga oshirildi! \n`);
   });
 } catch (err) {
-  console.log('⚠️ MongoDBga ulanishda xatolik:', err);
+  console.log("⚠️ MongoDBga ulanishda xatolik:", err);
 }
